838069a46fc9dd51497c27d2f152a53caf1bda0c
Magomed <sultanahmedov19948@yandex.ru>
2024-04-26 11:48:23 +0300

    Lesson_1

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import static org.slf4j.LoggerFactory.getLogger;
+import org.slf4j.Logger;
+
+public class UserServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("forward to users");
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/users.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mrmag
+  Date: 25.04.2024
+  Time: 13:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html lang="ru">
+<head>
+    <title>Users</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Users</h2>
+</body>
+</html>
